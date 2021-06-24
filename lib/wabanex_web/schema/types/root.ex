714531473ae838da1c6fc09d4eb12c9ddcd7ec82
Giovanni Sencioles <giovannisencioles@gmail.com>
2021-06-23 23:06:20 -0300

    Mutations for handling training and exercises info

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -1,10 +1,18 @@
 defmodule WabanexWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
-  alias WabanexWeb.Resolvers.User, as: UserResolver
+  alias WabanexWeb.Resolvers
+  alias WabanexWeb.Schema.Types
 
-  import_types WabanexWeb.Schema.Types.User
+  alias Resolvers.User, as: UserResolver
+  alias Resolvers.Training, as: TrainingResolver
+  alias Crudry.Middlewares.TranslateErrors
+
+  import_types Types.User
+  import_types Types.Training
+  import_types Types.Custom.UUID4
 
   @desc "Root query representation"
+
   object :root_query do
     field :get_user, type: :user do
       arg :id, non_null(:uuid4)
@@ -16,6 +24,12 @@ defmodule WabanexWeb.Schema.Types.Root do
     field :create_user, type: :user do
       arg :input, non_null(:create_user_input)
       resolve &UserResolver.create/2
+      middleware TranslateErrors
+    end
+    field :create_training, type: :training do
+      arg :input, non_null(:create_training_input)
+      resolve &TrainingResolver.create/2
+      middleware TranslateErrors
     end
   end
 end
